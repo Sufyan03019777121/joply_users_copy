e87685d14cc1380ba64676e72bf53df64ae978e1
Sufyan Ali <sufyanpakpatttan@mail.com>
2025-11-19 05:47:05 +0500

    260

--- src/pages/servicesOverview/ServicesOverview.jsx
+++ src/pages/servicesOverview/ServicesOverview.jsx
@@ -4,6 +4,9 @@ import "./servicesOverview.css";
 
 import Mask_group from "../../assets/images/Mask_group.png"
 import Find_the_Service from "../../assets/images/Find_the_Service.png"
+import rating_pic from "../../assets/images/rating_pic.png"
+import change_pic from "../../assets/images/change_pic.png"
+
 
 const { Title, Paragraph } = Typography;
 
@@ -20,22 +23,21 @@ const ServicesOverview = () => {
       ),
     },
     {
-      img: "https://via.placeholder.com/100",
+      img: rating_pic,
       title: "Connect with Professionals",
       desc: (
         <>
-          Post your job for free to connect with service providers. <br />
-          Providers interested in your job will send you a request.
+         Our rating system lets you read genuine reviews from previous
+         <br /> customers, helping you make informed decisions.
         </>
       ),
     },
     {
-      img: "https://via.placeholder.com/100",
+      img: change_pic,
       title: "Hire with Confidence",
       desc: (
         <>
-          Post your job for free to connect with service providers. <br />
-          Providers interested in your job will send you a request.
+         Review profiles, previous work, and ratings before deciding whom to <br /> contact. Only service providers you've selected can reach out to you.
         </>
       ),
     },
@@ -50,25 +52,25 @@ const ServicesOverview = () => {
         style={{ maxWidth: "1200px", margin: "0 auto" }}
       >
         {/* Left Side */}
-        <Col xs={24} md={14} className="left-section">
+        <Col xs={24} md={12} className="left-section">
           <Paragraph className="highlight-text">Now You Can</Paragraph>
-          <Title level={2} className="main-title">
-            Find Perfect Workers with Confidence
-          </Title>
-          <Paragraph className="description">
-            Finding the right worker isn't always easy. Joply is the dependable
-            solution to hire a suitable worker for your needs.
-          </Paragraph>
+          
+          <h1 className="main-title">
+            Find Perfect Workers with <br /> Confidence
+          </h1>
+          
+          <p className="description">
+             Finding the right worker isn't always easy. Joply is the dependable
+            solution <br /> to hire a suitable worker for your needs.
+          </p>
 
           {/* Services Rows */}
-          {/* {services.map((item, index) => (
+          {services.map((item, index) => (
             <div key={index} className="service-row">
               <div className="image-wrapper">
                 <Image
                   src={item.img}
                   alt={item.title}
-                  width={60}
-                  height={60}
                   style={{ objectFit: "cover" }}
                   preview={false}
                 />
@@ -78,14 +80,15 @@ const ServicesOverview = () => {
                 <p>{item.desc}</p>
               </div>
             </div>
-          ))} */}
+          ))}
         </Col>
 
         {/* Right Side */}
-        <Col xs={24} md={10} className="right-section">
+        <Col xs={24} md={12} className="right-section">
           <Card
             className="card-image"
-            cover={
+          >
+            <div className="services_image">
               <img
                 src={Mask_group}
                 alt="Workers"
@@ -95,10 +98,12 @@ const ServicesOverview = () => {
                   objectFit: "cover",
                 }}
               />
-            }
-          ></Card>
+                <div className="services-badge">50+ <br /> Services</div>
+            </div>
+           
+          </Card>
 
-          {/* <div className="services-badge">50+ Services</div> */}
+         
         </Col>
       </Row>
     </div>
